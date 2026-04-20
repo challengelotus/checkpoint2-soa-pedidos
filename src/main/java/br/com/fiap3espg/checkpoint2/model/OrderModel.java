f5a94228b542129c28e46a8056e31d54507b2dd0
jaoAprendiz <joaosoave94@gmail.com>
2026-04-20 18:49:42 -0300

    feat: adiciona anotações @Getter e @Setter do Lombok

--- src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
+++ src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
@@ -1,10 +1,13 @@
 package br.com.fiap3espg.checkpoint2.model;
 
 import jakarta.persistence.Entity;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Getter
+@Setter
 @Entity
 public class OrderModel {
     private Long id;
