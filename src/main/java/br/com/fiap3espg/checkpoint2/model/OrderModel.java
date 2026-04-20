8d7cd7afa7de8c9c65b1e94f62578f2b385f68a3
jaoAprendiz <joaosoave94@gmail.com>
2026-04-20 18:42:33 -0300

    feat: adiciona atributo orderDate (LocalDate) em OrderModel

--- src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
+++ src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
@@ -1,7 +1,10 @@
 package br.com.fiap3espg.checkpoint2.model;
 
+import java.time.LocalDate;
+
 public class OrderModel {
     private Long id;
     private String clientName;
+    private LocalDate orderDate;
 
 }
