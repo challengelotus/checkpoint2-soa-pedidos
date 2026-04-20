b7e0b0136377cba3f7ca933327ef188110c2c7e6
jaoAprendiz <joaosoave94@gmail.com>
2026-04-20 18:49:00 -0300

    feat: adiciona anotação @Entity à classe OrderModel

--- src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
+++ src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
@@ -1,8 +1,11 @@
 package br.com.fiap3espg.checkpoint2.model;
 
+import jakarta.persistence.Entity;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 public class OrderModel {
     private Long id;
     private String clientName;
