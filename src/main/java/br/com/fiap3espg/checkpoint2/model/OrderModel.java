6daa08f688175c83e945101ebef17deb4217d1fd
jaoAprendiz <joaosoave94@gmail.com>
2026-04-20 18:43:47 -0300

    feat: adiciona atributo totalValue (BigDecimal) em OrderModel

--- src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
+++ src/main/java/br/com/fiap3espg/checkpoint2/model/OrderModel.java
@@ -1,10 +1,12 @@
 package br.com.fiap3espg.checkpoint2.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class OrderModel {
     private Long id;
     private String clientName;
     private LocalDate orderDate;
+    private BigDecimal totalValue;
 
 }
