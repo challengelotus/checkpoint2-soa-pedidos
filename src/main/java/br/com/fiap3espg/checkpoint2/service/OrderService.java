3e42a11bb89922e4b5461933cf7caef81f8d3aae
Rafael T Lucena <rt.lucena@outlook.com>
2026-04-22 09:09:17 -0300

    feat(OrderService): add readAllOrders e o metodo readOrderById

--- src/main/java/br/com/fiap3espg/checkpoint2/service/OrderService.java
+++ src/main/java/br/com/fiap3espg/checkpoint2/service/OrderService.java
@@ -2,11 +2,25 @@ package br.com.fiap3espg.checkpoint2.service;
 
 import br.com.fiap3espg.checkpoint2.model.OrderModel;
 import br.com.fiap3espg.checkpoint2.repository.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class OrderService {
     private OrderRepository orderRepository;
 
     public OrderModel createOrder(OrderModel order) {
         return orderRepository.save(order);
+    }
+
+    public List<OrderModel> readAllOders() {
+        return orderRepository.findAll();
+    }
+
+    public OrderModel readOrderById(Long id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado!"));
     }
 }
