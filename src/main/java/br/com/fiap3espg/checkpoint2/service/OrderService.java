fb2e1ada94b513634f895ecf80285c2e43a6379e
Rafael T Lucena <rt.lucena@outlook.com>
2026-04-22 09:03:35 -0300

    feat: add orderRepository e o metodo createOrder in OrderService

--- src/main/java/br/com/fiap3espg/checkpoint2/service/OrderService.java
+++ src/main/java/br/com/fiap3espg/checkpoint2/service/OrderService.java
@@ -1,4 +1,12 @@
 package br.com.fiap3espg.checkpoint2.service;
 
+import br.com.fiap3espg.checkpoint2.model.OrderModel;
+import br.com.fiap3espg.checkpoint2.repository.OrderRepository;
+
 public class OrderService {
+    private OrderRepository orderRepository;
+
+    public OrderModel createOrder(OrderModel order) {
+        return orderRepository.save(order);
+    }
 }
