7efe6b146781564b5ac693a6cac124a075020e66
Rafael T Lucena <rt.lucena@outlook.com>
2026-04-22 09:15:37 -0300

    feat(OrderService): add updateOrder e o metodo deleteOrderById

--- src/main/java/br/com/fiap3espg/checkpoint2/service/OrderService.java
+++ src/main/java/br/com/fiap3espg/checkpoint2/service/OrderService.java
@@ -3,8 +3,8 @@ package br.com.fiap3espg.checkpoint2.service;
 import br.com.fiap3espg.checkpoint2.model.OrderModel;
 import br.com.fiap3espg.checkpoint2.repository.OrderRepository;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -22,5 +22,23 @@ public class OrderService {
     public OrderModel readOrderById(Long id) {
         return orderRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado!"));
+    }
+
+    public OrderModel updateOrder(Long id, OrderModel order) {
+        return orderRepository.findById(id)
+                .map(existingOrder -> {
+                    existingOrder.setClientName(order.getClientName());
+                    existingOrder.setTotalValue(order.getTotalValue());
+                    return orderRepository.save(existingOrder);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado."));
+    }
+
+    public void deleteOrderById(Long id) {
+        try {
+            orderRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("Pedido não encontrado.");
+        }
     }
 }
